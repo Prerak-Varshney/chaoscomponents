f1751f13d61ebf38e3facc8b8c85397729749e60
Prerak Varshney <e22cseu0138@bennett.edu.in>
2025-03-05 23:46:14 +0530

    Tailwind not working

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-// import { ChaosComponents } from '../';
+import { ChaosComponents } from '../';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <ChaosComponents> */}
+    <ChaosComponents>
       <App />
-    {/* </ChaosComponents> */}
+    </ChaosComponents>
   </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
