35d9d362b88888e258710988264cce0922b01857
Prerak Varshney <e22cseu0138@bennett.edu.in>
2025-03-06 16:43:59 +0530

    Fixes

--- lib/components/ChaosComponents/ChaosComponents.tsx
+++ lib/components/ChaosComponents/ChaosComponents.tsx
@@ -6,7 +6,7 @@ interface ChaosComponentsProps {
 
 const ChaosComponents = ({ children }: ChaosComponentsProps) => {
     return (
-        <div className={`w-full min-h-screen bg-black`}>
+        <div className={`w-full min-h-screen bg-white`}>
             {children}
         </div>
     )
