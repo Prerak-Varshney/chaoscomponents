2963081bae853a4647d4caed440a8a9cd34a83ff
Prerak Varshney <e22cseu0138@bennett.edu.in>
2025-03-07 20:22:06 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Button, Input } from '../';
 
 const App = () => {
+  const [value, setValue] = React.useState<string>('');
   return (
     <div className='text-blue-500 bg-black w-full h-screen flex flex-col items-center justify-center gap-4'>
       Hello World
       <Button label='Click me' hoverBgColor='#09f' />
-      <Input placeholder='Type here' buttonStyle='google' />
+      <Input placeholder='Type here' buttonStyle='google' value={value} setValue={setValue} />
     </div>
   )
 }
